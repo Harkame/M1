4c3c200016228b2ddb56f0f63cda404a6b83b955
Harkame <louisr.daviaud@gmail.com>
2017-10-03 12:31:59 +0200

    Fix

--- ingenierie_logiciel/tp_2/src/TestDictionary.java
+++ ingenierie_logiciel/tp_2/src/TestDictionary.java
@@ -7,58 +7,9 @@ import org.junit.Test;
 
 public class TestDictionary
 {
-	private TestOrderedDictionary a_test_ordered_dictionary;
-	private TestFastDictionary a_test_fast_dictionary;
-	private TestSortedDictionary a_test_sorted_dictionary;
-	
-	@Before
-	public void setUp() throws Exception
-	{
-		a_test_ordered_dictionary = new TestOrderedDictionary();
-		a_test_fast_dictionary = new TestFastDictionary();
-		a_test_sorted_dictionary = new TestSortedDictionary();
-	}
-	
-	
-	
-	@Test
-	public void testEmpty()
-	{
-		a_test_ordered_dictionary.testEmpty();
-		a_test_fast_dictionary.testEmpty();
-		a_test_sorted_dictionary.testEmpty();
-	}
-	
-	@Test
-	public void testSize()
-	{
-		a_test_ordered_dictionary.testSize();
-		a_test_fast_dictionary.testSize();
-		a_test_sorted_dictionary.testSize();
-	}
-	
-	@Test
-	public void testAddElements()
-	{
-		a_test_ordered_dictionary.testAddElements();
-		a_test_fast_dictionary.testAddElements();
-		a_test_sorted_dictionary.testAddElements();
-	}
-	
-	@Test
-	public void testAddSameElements()
-	{
-		a_test_ordered_dictionary.testAddSameElements();
-		a_test_fast_dictionary.testAddSameElements();
-		a_test_sorted_dictionary.testAddSameElements();
-	}
-	
-	@Test
-	public void testGetElement()
-	{	
-		a_test_ordered_dictionary.testGetElement();
-		a_test_fast_dictionary.testGetElement();
-		a_test_sorted_dictionary.testGetElement();
-	}
-	
+	@Suite.SuiteClasses({
+		   TestOrderedDictionary.class,
+		   TestFastDictionary.class,
+		   TestSortedDictionary
+		})
 }
