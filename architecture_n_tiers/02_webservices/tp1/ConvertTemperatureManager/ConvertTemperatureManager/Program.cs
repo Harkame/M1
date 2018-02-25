1f6de403054df43423eaa41595379e5d9dbb4764
Harkame <louisr.daviaud@gmail.com>
2018-02-25 15:26:00 +0100

    Clear

---
+++ architecture_n_tiers/02_webservices/tp1/ConvertTemperatureManager/ConvertTemperatureManager/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConvertTemperatureManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class Program
+    {
+        private static ConvertTemperatureServiceReference.ConvertTemperatureSoapClient a_proxy;
+
+        static void Main(string[] args)
+        {
+            a_proxy = new ConvertTemperatureServiceReference.ConvertTemperatureSoapClient();
+
+            Console.WriteLine("Convertor Celsius -> Kelvin");
+
+            Console.Write("Temperature (Celsius) (Example : 25,9) : ");
+
+            double t_temperature_to_convert = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine(a_proxy.ConvertTemp(t_temperature_to_convert, ConvertTemperatureServiceReference.TemperatureUnit.degreeCelsius, ConvertTemperatureServiceReference.TemperatureUnit.kelvin) + " kelvin");
+
+            Console.WriteLine(Environment.NewLine + "Please, press enter to continue");
+
+            Console.ReadLine();
+
+            a_proxy.Close();
+        }
+    }
+}
