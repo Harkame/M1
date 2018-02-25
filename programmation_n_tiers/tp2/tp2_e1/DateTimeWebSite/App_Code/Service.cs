de88bba65003cc76414d7765bfecf2654069825f
Harkame <louisr.daviaud@gmail.com>
2018-02-25 13:48:50 +0100

    e1

---
+++ programmation_n_tiers/tp2/tp2_e1/DateTimeWebSite/App_Code/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+
+public class Service : System.Web.Services.WebService
+{
+    public Service ()
+    {
+    }
+
+    [WebMethod]
+    public DateTime DateTimeNow()
+    {
+        return DateTime.Now;
+    }
+}
