092033d2331681d6daae2c796cdcb47026b6cff7
Harkame <louisr.daviaud@gmail.com>
2018-03-05 11:30:28 +0100

    Fix

---
+++ architecture_n_tiers/02_webservices/tp3/e2/LibraryManager/LibraryManager/Controllers/SubscriberController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryManager.Models;
+
+namespace LibraryManager.Controllers
+{
+    public class SubscriberController : ApiController
+    {
+        SubscriberServiceContext db = new SubscriberServiceContext();
+
+        public IEnumerable<Subscriber> Get()
+        {
+            return Library.a_subscribers.ToArray();
+        }
+
+        public Subscriber Get(int p_id)
+        {
+            foreach (Subscriber t_subscriber in Library.a_subscribers)
+                if (t_subscriber.ID == p_id)
+                     return t_subscriber;
+
+            return null;
+        }
+
+        [HttpPost, Route("api/subscriber")]
+        public void Post([FromBody]Subscriber subscriber)
+        {
+            throw new Exception(subscriber.Password);
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int p_id)
+        {
+
+        }
+    }
+}
---
+++ architecture_n_tiers/02_webservices/tp3/e2/LibraryManager/LibraryManager/Models/Comment.cs
@@ -0,0 +1,51 @@
+﻿namespace LibraryManager.Models
+{
+    using System;
+    using System.Linq;
+    using System.Web;
+    using System.Text;
+
+    public class Comment
+    {
+        public User a_User;
+        public String a_description;
+        public User User
+        {
+            get
+            {
+                return a_User;
+            }
+            set
+            {
+                a_User = value;
+            }
+        }
+        public String Description
+        {
+            get
+            {
+                return a_description;
+            }
+            set
+            {
+                a_description = value;
+            }
+        }
+        public Comment()
+        {
+        }
+        public Comment(User p_User, String p_description)
+        {
+            a_User = p_User;
+            a_description = p_description;
+        }
+        public override String ToString()
+        {
+            StringBuilder r_to_string = new StringBuilder();
+            r_to_string.Append(a_User.ID);
+            r_to_string.Append(" - ");
+            r_to_string.Append(a_description);
+            return r_to_string.ToString();
+        }
+    }
+}
