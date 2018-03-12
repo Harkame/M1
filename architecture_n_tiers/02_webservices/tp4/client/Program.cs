1ef27515d3a6c37a8fc303c521a3ab3ffde6b942
Harkame <louisr.daviaud@gmail.com>
2018-03-12 11:36:12 +0100

    Fix

---
+++ architecture_n_tiers/02_webservices/tp4/client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.IService1 serviceProxy = new ChannelFactory<ServiceReference1.IService1>("WSHttpBinding_IService1").CreateChannel();
+            Console.WriteLine(serviceProxy.GetData(1));
+            Console.ReadLine();
+        }
+    }
+}
