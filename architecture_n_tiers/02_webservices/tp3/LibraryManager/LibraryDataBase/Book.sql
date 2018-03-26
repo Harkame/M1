34adc5fa8f2fb4ba27994990014387dacc89e78f
Harkame <louisr.daviaud@gmail.com>
2018-03-26 02:08:17 +0200

    Fix

---
+++ architecture_n_tiers/02_webservices/tp3/LibraryManager/LibraryDataBase/Book.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Book]
+(
+	[isbn] INT NOT NULL PRIMARY KEY IDENTITY (0, 1),
+	[title] NCHAR(25) NULL, 
+    [author] NCHAR(25) NULL, 
+    [stock] INT NULL, 
+    [editor] NCHAR(25) NULL
+)
