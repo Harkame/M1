e7c2a2102efd4eb6c5637666435869e7349a927b
Daviaud Louis Richard <louis-richard.daviaud@etu.umontpellier.fr>
2018-03-30 15:06:16 +0200

    Fix

--- architecture_n_tiers/02_webservices/tp3/LibraryManager/LibraryDataBase/Book.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Book]
-(
-	[isbn] INT NOT NULL PRIMARY KEY IDENTITY (0, 1),
-	[title] NCHAR(25) NULL, 
-    [author] NCHAR(25) NULL, 
-    [stock] INT NULL, 
-    [editor] NCHAR(25) NULL
-)
